f8d16ac55d426ce8cb4a57bc7674fcbfdeca8923
Sylwia <sylwia.tomczak@awesomebytes.pl>
2021-09-13 11:05:31 +0200

    Implemented BoatTitle.cmp, BoatTitle.css and BoatTitle controller class.

--- force-app/main/default/aura/Challenge3/BoatTile/BoatTileController.js
+++ force-app/main/default/aura/Challenge3/BoatTile/BoatTileController.js
@@ -1,5 +1,9 @@
 ({
-    myAction : function(component, event, helper) {
-
+    //inicjowanie funkcji przekazanej z component
+    doInit : function(component, event, helper) {
+        //zmienna data pobrana z componentu zmiennej boat
+        var data = component.get("v.boat");
+        //wyświetlenie loga z data w logach
+        console.log("data received " + data);
     }
 })
